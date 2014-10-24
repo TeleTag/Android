a0e0e2b78082155100175d62e366318bfe76d1f2
DKByo <danyel.nerv@gmail.com>
2014-10-23 22:02:50 -0500

    Initial

---
+++ TeleTag/src/com/dkbyo/teletag/MapFragment.java
@@ -0,0 +1,37 @@
+package com.dkbyo.teletag;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+
+	private static final String ARG_SECTION_NUMBER = "section_number";
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View v = inflater.inflate(R.layout.fragment_map, container);
+		return v;
+	}
+	
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		((MainActivity) activity).onSectionAttached(getArguments().getInt(
+				ARG_SECTION_NUMBER));
+	}
+	
+	public static MapFragment newInstance(int section) {
+		MapFragment fragment = new MapFragment();
+		Bundle args = new Bundle();
+		args.putInt(ARG_SECTION_NUMBER, section);
+		fragment.setArguments(args);
+		return fragment;
+	}
+}
